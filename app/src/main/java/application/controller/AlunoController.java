d8efdbdd172c66be2d19667cda8fa1d7308e96ef
ViniciusMendonca12 <vivicomferreira@hotmail.com>
2024-08-13 19:52:30 +0000

    jkj

---
+++ app/src/main/java/application/controller/AlunoController.java
@@ -0,0 +1,31 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.model.Aluno;
+import application.repository.AlunoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    @Autowired
+    private AlunoRepository alunoRepo;
+
+    @GetMapping("/alunos")
+    public Iterable<Aluno> list(){
+        return alunoRepo.findAll();
+
+    }
+
+    @PostMapping("/aluno")
+    public Aluno insert(@RequestBody Aluno aluno){
+        return alunoRepo.save(aluno);
+    }
+}
---
+++ app/src/main/java/application/model/Aluno.java
@@ -0,0 +1,38 @@
+package application.model;
+
+import javax.annotation.processing.Generated;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+
+@Entity
+@Table(name = "alunos")
+public class Aluno{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+}
